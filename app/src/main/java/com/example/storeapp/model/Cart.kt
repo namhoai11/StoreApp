26285e0ab0c43b3950d72a5af1feb42df547428e
LaHoaiNam <20521629@gm.uit.edu.vn>
2025-01-19 23:05:10 +0700

    Fix Model

--- app/src/main/java/com/example/storeapp/model/Cart.kt
+++ app/src/main/java/com/example/storeapp/model/Cart.kt
@@ -1,30 +1,13 @@
 package com.example.storeapp.model
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 
-@Entity
 data class Cart(
-    @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name = "id")
     var id: Int = 0,
-
-    @ColumnInfo(name = "product_id")
-    var productId: Int,
-
-    @ColumnInfo(name = "product_name")
-    var productName: String,
-
-    @ColumnInfo(name = "product_price")
-    var productPrice: String,
-
-    @ColumnInfo(name = "product_image")
-    var productImage: String,
-
-    @ColumnInfo(name = "product_category")
-    var productCategory: String,
-
-    @ColumnInfo(name = "product_quantity")
-    var productQuantity: Int,
+    var productId: Int = 0,
+    var productName: String = "",
+    var productPrice: String = "",
+    var productImage: String = "",
+    var productCategory: String = "",
+    var productQuantity: Int = 0
 )
+
